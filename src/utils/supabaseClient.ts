73fdf6f8dc00020ee59cd47bf5f5047a183e7266
Francesco Calò <fcaloisme@gmail.com>
2023-08-14 08:55:44 +0200

    Wiring profile and login pages

---
+++ src/utils/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+
+// to be used on client
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
