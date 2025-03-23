ef854684ee52e13403adb3a8e1f0884a8caa6dd5
Francesco Calò <fcaloisme@gmail.com>
2025-03-23 11:06:01 +0100

    Outsource appMailRedirects value

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,5 +3,7 @@ export const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
 export const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 export const cookiePrefix = import.meta.env.PUBLIC_SUPABASE_COOKIE_PRE
 
+export const appMailRedirects  = import.meta.env.PUBLIC_APP_URL
+
 export const accessTokenName = `${import.meta.env.PUBLIC_SUPABASE_COOKIE_PRE}-access-token`
 export const refreshTokenName = `${import.meta.env.PUBLIC_SUPABASE_COOKIE_PRE}-refresh-token`
