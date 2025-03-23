391700bf01c510a717479e728b42d671453d7515
Francesco Calò <fcaloisme@gmail.com>
2025-03-23 10:34:14 +0100

    Use netlify adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: netlify(),
+  //adapter: node({
+  //  mode: 'standalone'
+  //})
 });
